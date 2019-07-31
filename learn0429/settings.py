905f6751dc767ff5557095aed51d85b87412edf0
wonderjungin <50102738+wonderjungin@users.noreply.github.com>
2019-07-31 13:02:21 +0900

    Update settings.py

--- learn0429/settings.py
+++ learn0429/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*c3z0cd00f^+0hyad7fjf$6p+@dy&duk-(^t!!1n8*!)3byv5h'
+#SECRET_KEY = '*c3z0cd00f^+0hyad7fjf$6p+@dy&duk-(^t!!1n8*!)3byv5h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
